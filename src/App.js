825366baf503f482e091e0a55aae48e023ee14b6
axel <axel.bouillart@gmail.com>
2017-01-04 14:23:22 +0100

    Ajout Alerte

--- src/App.js
+++ src/App.js
@@ -4,52 +4,49 @@ import './App.css';
 
 class App extends Component {
 
-constructor(){
-  super();
-  this.state = {
-    counter: 0,
-    stack: ['hey','ho','hi','ah'],
-  }
-}
-
-
-click(){
-this.setState({
-  counter : this.state.counter +1,
-});
-
- console.log('Sucess')
- this.random();
-
-  }
-random(){
-
-let randomize = Math.floor((Math.random()* 5));
-
-if (this.counter % 20 === 0){
-      
-      let nbalea = this.random();
-      let randomMes = this.stack[nbalea];
-      alert(randomMes);
-
+  constructor(){
+    super();
+    this.state = {
+      counter: 0,
+      stack: ['hey','ho','hi','ah'],
     }
+  }
 
 
-} 
+  click(){
+    this.setState({
+      counter : this.state.counter +1,
+    });
+    this.random();
 
-render() {
-  return (
-    <div className="App">
-    <div className="App-header">
-    <img onClick={this.click.bind(this)} id="logoclick" src={logo} className="App-logo" alt="logo" />
-    <h2 className="titleh2" id="titleh">{this.state.counter}</h2>
-    </div>
-    <div className="timerSpan">
+    console.log('Sucess')
 
-    </div>
-    </div>
-    );
-}
+  }
+  random(){
+
+    if (this.state.counter % 10 === 0){
+      let rand = this.state.stack;
+      alert(rand[Math.floor(Math.random()* rand.length)]);
+      
+
+    };
+
+
+  } 
+
+  render() {
+    return (
+      <div className="App">
+      <div className="App-header">
+      <img onClick={this.click.bind(this)} id="logoclick" src={logo} className="App-logo" alt="logo" />
+      <h2 className="titleh2" id="titleh">{this.state.counter}</h2>
+      </div>
+      <div className="timerSpan">
+
+      </div>
+      </div>
+      );
+  }
 }
 
 
