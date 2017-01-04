acc3fbae94beaaf263de9d2149c3d271c1a2db90
axel <axel.bouillart@gmail.com>
2017-01-04 12:09:54 +0100

    Cookie clickable & chiffrer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,54 @@ import logo from './cookie.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <div className="App-header">
-      <img onClick="#" src={logo} className="App-logo" alt="logo" />
-      <h2 className="titleh2">0</h2>
-      </div>
-      <div className="timerSpan">
-    
-      </div>
-      </div>
-      );
-    }
-  }
 
-  export default App;
+constructor(){
+  super();
+  this.state = {
+    counter: 0,
+    stack: ['hey','ho','hi','ah'],
+  }
+}
+
+
+click(){
+this.setState({
+  counter : this.state.counter +1,
+});
+
+ console.log('Sucess')
+ this.random();
+
+  }
+random(){
+
+let randomize = Math.floor((Math.random()* 5));
+
+if (this.counter % 20 === 0){
+      
+      let nbalea = this.random();
+      let randomMes = this.stack[nbalea];
+      alert(randomMes);
+
+    }
+
+
+} 
+
+render() {
+  return (
+    <div className="App">
+    <div className="App-header">
+    <img onClick={this.click.bind(this)} id="logoclick" src={logo} className="App-logo" alt="logo" />
+    <h2 className="titleh2" id="titleh">{this.state.counter}</h2>
+    </div>
+    <div className="timerSpan">
+
+    </div>
+    </div>
+    );
+}
+}
+
+
+export default App;
